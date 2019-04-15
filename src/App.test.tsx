7bff49a34735b54ba6c883bae7f4acc7e73d9ff5
Anton Charnou <anton.charnou@itechart-group.com>
2019-04-15 06:47:20 +0300

    feat: connect THREE.js globe

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+jest.mock('./texturedGlobe.ts');
+
 it('renders without crashing', (): void => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
-import logo from './logo.svg';
+import init from './texturedGlobe';
 
 class App extends Component {
+  public componentDidMount(): void {
+    init();
+  }
+
   public render(): React.ReactNode {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <header className="App-header">CLEAN PLANET</header>
       </div>
     );
   }
