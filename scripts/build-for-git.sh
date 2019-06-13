ba72e05e81b07ed3e1aa94091d40a8f1198c2b86
Anton Chernov <chernov.anton.dev@gmail.com>
2019-06-13 06:50:03 +0300

    feat: continue playing with build script

--- scripts/build-for-git.sh
+++ scripts/build-for-git.sh
@@ -1,6 +1,9 @@
 #!/bin/bash -e
 
 rm -rf tmp
+rm -rf deploy
+git clone git@github.com:chernov-anton/clean-planet.git deploy
+cd deploy
 git checkout clean-planet@1.0
 npm i
 cp .env.example .env.local
@@ -8,8 +11,8 @@ sed -i -e "s,<PUBLIC_URL>,/clean-planet/globe," .env.local
 npm run build
 mkdir -p tmp
 mv build/* tmp
-mkdir -p deploy/globe
-mv tmp/* deploy/globe
+mkdir -p build-for-gh/globe
+mv tmp/* build-for-gh/globe
 rm -rf tmp
 
 git checkout data-planet@1.0
@@ -19,6 +22,7 @@ sed -i -e "s,<PUBLIC_URL>,/clean-planet/data," .env.local
 npm run build
 mkdir -p tmp
 mv build/* tmp
-mkdir -p deploy/data
-mv tmp/* deploy/data
+mkdir -p build-for-gh/data
+mv tmp/* build-for-gh/data
 rm -rf tmp
+cd ../
