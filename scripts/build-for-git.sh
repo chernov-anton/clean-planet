0523c75f66ca7fe425139ecaf198749d58619987
Anton Chernov <chernov.anton.dev@gmail.com>
2019-06-13 06:32:45 +0300

    feat: playing with build script

--- scripts/build-for-git.sh
+++ scripts/build-for-git.sh
@@ -1,11 +1,24 @@
 #!/bin/bash -e
 
+rm -rf tmp
+git checkout clean-planet@1.0
+npm i
 cp .env.example .env.local
 sed -i -e "s,<PUBLIC_URL>,/clean-planet/globe," .env.local
 npm run build
 mkdir -p tmp
 mv build/* tmp
-mkdir -p build/globe
-mv tmp/* build/globe
+mkdir -p deploy/globe
+mv tmp/* deploy/globe
 rm -rf tmp
 
+git checkout data-planet@1.0
+npm i
+cp .env.example .env.local
+sed -i -e "s,<PUBLIC_URL>,/clean-planet/data," .env.local
+npm run build
+mkdir -p tmp
+mv build/* tmp
+mkdir -p deploy/data
+mv tmp/* deploy/data
+rm -rf tmp
