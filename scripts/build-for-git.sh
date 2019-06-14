7ad6403c8b6edd1a5b43185fff3fc031ed9580ad
Anton Chernov <chernov.anton.dev@gmail.com>
2019-06-14 22:34:58 +0300

    infra: deploy new version

--- scripts/build-for-git.sh
+++ scripts/build-for-git.sh
@@ -3,6 +3,8 @@
 rm -rf deploy
 git clone git@github.com:chernov-anton/clean-planet.git deploy
 cd deploy
+
+#build of globe with cloud
 git checkout clean-planet@1.0
 npm i
 cp .env.example .env.local
@@ -11,12 +13,13 @@ npm run build
 mkdir -p build-for-gh/globe
 mv build/* build-for-gh/globe
 
-git checkout data-planet@1.0
+#build of clickable globe with data
+git checkout data-planet@1.1
 npm i
 cp .env.example .env.local
 sed -i -e "s,<PUBLIC_URL>,/clean-planet/data," .env.local
 npm run build
 mkdir -p build-for-gh/data
 mv build/* build-for-gh/data
-rm -rf tmp
+
 cd ../
