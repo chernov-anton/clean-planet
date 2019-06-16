d600cb4f3172078161ec3e4fcda9885aa280a163
Anton Chernov <chernov.anton.dev@gmail.com>
2019-06-16 19:14:07 +0300

    Deploy to gh pages

---
+++ globe/precache-manifest.9b8c66af54ee5b3e597b72bb652a5051.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b486305cf484d471148cd05f856f3e9",
+    "url": "/clean-planet/globe/index.html"
+  },
+  {
+    "revision": "8e3682203bae559b1884",
+    "url": "/clean-planet/globe/static/css/main.ee49a795.chunk.css"
+  },
+  {
+    "revision": "bfe9994ac268beb85d63",
+    "url": "/clean-planet/globe/static/js/2.c0b3312f.chunk.js"
+  },
+  {
+    "revision": "8e3682203bae559b1884",
+    "url": "/clean-planet/globe/static/js/main.445e62e1.chunk.js"
+  },
+  {
+    "revision": "4e0364ec65c96f2370b1",
+    "url": "/clean-planet/globe/static/js/runtime~main.bc32a245.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/clean-planet/data/precache-manifest.b6af4359e6dc548a337396273f8f0e5e.js"
+  "/clean-planet/precache-manifest.59ecd0d1467b83a52954de0821506ec4.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/clean-planet/data/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/clean-planet/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
